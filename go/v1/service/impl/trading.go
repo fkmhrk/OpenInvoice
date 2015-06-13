922a50a388efe3ebd9bb3fd76d5392a60262dfa8
hrk fkm <fkm@fkmsoft.jp>
2015-06-13 20:31:58 +0900

    Add modified_time

--- go/v1/service/impl/trading.go
+++ go/v1/service/impl/trading.go
@@ -288,5 +288,6 @@ func (s *tradingService) toJson(t *m.Trading) map[string]interface{} {
 		"tax_rate":         t.TaxRate,
 		"assignee":         t.AssigneeId,
 		"product":          t.Product,
+		"modified_time":    t.ModifiedTime,
 	}
 }
