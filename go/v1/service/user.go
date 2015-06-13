17f5955fa04ea51973f68ef65e63c4dc589380e7
hrk fkm <fkm@fkmsoft.jp>
2015-06-14 00:45:55 +0900

    Add refresh token API

--- go/v1/service/user.go
+++ go/v1/service/user.go
@@ -2,6 +2,7 @@ package service
 
 type UserService interface {
 	GetToken(name, pass string) Result
+	RefreshToken(token string) Result
 	GetUsers(token string) Result
 	Create(token, loginName, displayName, role, password string) Result
 }
