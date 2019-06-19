0c6a6e44c43ad1dad127d6d92dda30701ae596a4
hrk fkm <fkm@mokelab.com>
2019-06-19 11:34:32 +0900

    Add addSnack() API

--- go/web/src/IApplication.ts
+++ go/web/src/IApplication.ts
@@ -4,6 +4,8 @@ interface IApplication {
     start(): void;
     navigate(path: string): void;
 
+    addSnack(item: string): void;
+
     showDialog(dialog: IDialog): void;
 
     closeDialog(dialog: IDialog): void;
